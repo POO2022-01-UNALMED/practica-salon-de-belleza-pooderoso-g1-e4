310302d4e7d152c6492e8494b5333e57e223a663
Julian Londono <julondonor@unal.edu.co>
2022-05-09 15:05:01 -0500

    Se completaron las clases Cita y Factura y se agrego la clase Servicio

--- gestorAplicacion/operacional/Cita.java
+++ gestorAplicacion/operacional/Cita.java
@@ -1,5 +1,81 @@
 package operacional;
+import java.util.ArrayList;
+import organizacional.*;
+import java.time.*;
 
 public class Cita {
-
+	private int idCita;
+	private Empleado empleado;
+	private Cliente cliente;
+	private ArrayList<Servicio> servicios = new ArrayList<Servicio>();
+	private LocalDateTime fechaReserva; //año, mes, dia, hora y minutos
+	private LocalDateTime fechaCita;
+	private int duracion; //minutos
+	private Factura factura;
+	public static int NumCitas=0;
+	
+	
+	public Cita(Empleado empleado, Cliente cliente, ArrayList<Servicio> servicios, LocalDateTime fechaReserva, LocalDateTime fechaCita, int duracion) {
+		Cita.NumCitas ++;
+		this.idCita = Cita.NumCitas;
+		this.empleado = empleado; this.cliente = cliente; this.servicios = servicios;
+		this.fechaReserva = fechaReserva; this.fechaCita = fechaCita; this.duracion = duracion; 
+	}
+	
+	
+	//Metodos get y set:
+	public int getId() {
+		return this.idCita;
+	}
+	public void setId(int id) {
+		this.idCita = id;
+	}
+	
+	public Empleado getEmpleado() {
+		return this.empleado;
+	}
+	public void setEmpleado(Empleado empleado) {
+		this.empleado = empleado;
+	}
+	
+	public Cliente getCliente() {
+		return this.cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public ArrayList<Servicio> getServicios(){
+		return this.servicios;
+	}
+	public void setServicios(ArrayList<Servicio> servicios) {
+		this.servicios = servicios;
+	}
+	
+	public LocalDateTime getFechaReserva() {
+		return this.fechaReserva;
+	}
+	public void setFechaReserva(LocalDateTime fechaReserva) {
+		this.fechaReserva = fechaReserva;
+	}
+	public LocalDateTime getFechaCita() {
+		return this.fechaCita;
+	}
+	public void setFechaCita(LocalDateTime fechaCita) {
+		this.fechaCita = fechaCita;
+	}
+	
+	public int getDuracion() {
+		return this.duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	
+	public Factura getFactura() {
+		return this.factura;
+	}
+	public void setFactura(Factura factura) {
+		this.factura = factura;
+	}
 }
--- gestorAplicacion/operacional/Factura.java
+++ gestorAplicacion/operacional/Factura.java
@@ -1,5 +1,57 @@
 package operacional;
+import java.util.ArrayList;
+import organizacional.*;
+import java.time.*;
 
 public class Factura {
-
+	private int idFactura;
+	private Cita cita;
+	private double precioTotal;
+	private LocalDateTime fecha;
+	private String metodoPago;
+	public static int NumFacturas=0;
+	
+	public Factura(Cita cita, LocalDateTime fecha, String metodoPago) {
+		Factura.NumFacturas++;
+		this.idFactura = Factura.NumFacturas;
+		this.cita = cita;
+		this.fecha = fecha;
+		this.metodoPago = metodoPago;
+	}
+	
+	//Metodos get y set:
+	public int getIdFactura() {
+		return this.idFactura;
+	}
+	public void setIdFactura(int idFactura) {
+		this.idFactura = idFactura;
+	}
+	
+	public Cita getCita() {
+		return this.cita;
+	}
+	public void setCita(Cita cita) {
+		this.cita = cita;
+	}
+	
+	public double getPrecioTotal() {
+		return this.precioTotal;
+	}
+	public void setPrecioTotal(double precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	
+	public LocalDateTime getFecha() {
+		return this.fecha;
+	}
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+	
+	public String getMetodoPago() {
+		return this.metodoPago;
+	}
+	public void setMetodoPago(String metodoPago) {
+		this.metodoPago = metodoPago;
+	}
 }
