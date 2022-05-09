d7995ef77d53bd33c602120bbb651dc5254ec774
judhenaoma <68240004+judhenaoma@users.noreply.github.com>
2022-05-09 17:53:04 -0500

    Clase Empleado, Servicio y Producto agregadas

--- gestorAplicacion/operacional/Producto.java
+++ gestorAplicacion/operacional/Producto.java
@@ -1,5 +1,37 @@
 package operacional;
 
 public class Producto {
-
+	private String productoId;
+	private int existencias;
+	private String nombreProducto;
+	private float precioVenta;
+	
+	public String getProductoId() {
+		return productoId;
+	}
+	public void setProductoId(String productoId) {
+		this.productoId = productoId;
+	}
+	public int getExistencias() {
+		return existencias;
+	}
+	public void setExistencias(int existencias) {
+		this.existencias = existencias;
+	}
+	public String getNombreProducto() {
+		return nombreProducto;
+	}
+	public void setNombreProducto(String nombreProducto) {
+		this.nombreProducto = nombreProducto;
+	}
+	public float getPrecioVenta() {
+		return precioVenta;
+	}
+	public void setPrecioVenta(float precioVenta) {
+		this.precioVenta = precioVenta;
+	}
+	
+	
+	
+	
 }
--- gestorAplicacion/operacional/Servicio.java
+++ gestorAplicacion/operacional/Servicio.java
@@ -1,5 +1,23 @@
 package operacional;
 
-public class Servicio {
+public enum Servicio {
+	
+	PEDICURE (23000),
+	MANICURE (17000),
+	ALIZADO (30000),
+	CEPILLADO (27000),
+	KERATINA (55000),
+	CORTE_CABALLERO (16000),
+	CORTE_DAMA (21000),
+	EXFOLIACION_FACIAL (11000),
+	CEJAS (13000),
+	DEPILACION_LASER (107000);
+	
+	private final float precio;
+	
+	Servicio(float precio){
+		this.precio = precio;
+	}
+	
 
 }
