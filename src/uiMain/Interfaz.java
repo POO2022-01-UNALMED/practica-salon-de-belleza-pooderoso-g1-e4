37a4024f1ba9edea30328f3e594431a89920b520
judhenaoma <68240004+judhenaoma@users.noreply.github.com>
2022-05-19 15:56:35 -0500

    Ajuste clase Interfaz

--- src/uiMain/Interfaz.java
+++ src/uiMain/Interfaz.java
@@ -13,8 +13,8 @@ import gestorAplicacion.operacional.*;
 public class Interfaz {
 	public static void main(String[] args) {
 		Scanner entrada =new Scanner(System.in);
-		Empleado empleado1 =new Empleado("Marlon", "Nivia", 1000349061, 21, 32088456, "U�as", null, null);
-		Empleado empleado2 =new Empleado("Julian", "Ospina", 525206530, 20, 3208844, "Pelo", null, null);//El numero no alcanza parce
+		Empleado empleado1 =new Empleado("Marlon", "Nivia", 1000349061, 21, 32088456, "U�as");
+		Empleado empleado2 =new Empleado("Julian", "Ospina", 525206530, 20, 3208844, "Pelo");//El numero no alcanza parce
 		Administrador SuperAdministador =new Administrador("Juan","Cuadrado",123,21,444444,"12-2",2222);
 		int opcion = 0;
 		do {
