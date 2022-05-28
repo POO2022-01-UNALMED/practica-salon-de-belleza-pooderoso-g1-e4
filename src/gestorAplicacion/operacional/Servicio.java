0afc4855ff3e30e20268aba7981480bc9569cfc3
Marlon <mnivia@unal.edu.co>
2022-05-28 13:25:01 -0500

    duracion de Citas en Servcio

--- src/gestorAplicacion/operacional/Servicio.java
+++ src/gestorAplicacion/operacional/Servicio.java
@@ -2,22 +2,33 @@ package gestorAplicacion.operacional;
 
 public enum Servicio {
 	
-	PEDICURE (23000),
-	MANICURE (17000),
-	ALIZADO (30000),
-	CEPILLADO (27000),
-	KERATINA (55000),
-	CORTE_CABALLERO (16000),
-	CORTE_DAMA (21000),
-	EXFOLIACION_FACIAL (11000),
-	CEJAS (13000),
-	DEPILACION_LASER (107000);
+	PEDICURE (23000,60),
+	MANICURE (17000,60),
+	ALIZADO (30000,60),
+	CEPILLADO (27000,60),
+	KERATINA (55000,60),
+	CORTE_CABALLERO (16000,30),
+	CORTE_DAMA (21000,45),
+	EXFOLIACION_FACIAL (11000,60),
+	CEJAS (13000,30),
+	DEPILACION_LASER (107000,80);
 	
 	private final float precio;
+	private int duracion;
 	
-	Servicio(float precio){
+	private Servicio(float precio,int duracion){
 		this.precio = precio;
+		this.duracion = duracion;
 	}
+	
+	public float getPrecio() {
+		return precio;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}	
+	
 	
 
 }
